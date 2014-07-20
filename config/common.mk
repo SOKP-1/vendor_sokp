866d19c110ad2b07ba13aa974d5712280fd33912
Audio God <audiogod@live.com>
2014-07-20 14:03:06 -0400

    Build SOKP MILESTONE-04

--- config/common.mk
+++ config/common.mk
@@ -333,7 +333,7 @@ else
     endif
 endif
 
-SOKP_Version=SS-01-KK444-KTU84Q
+SOKP_Version=MS-04-KK444-KTU84Q
 SOKP_MOD_VERSION := SOKP-$(SOKP_Version)-$(shell date -u +%Y%m%d)$(SOKP_EXTRAVERSION)-$(SOKP_BUILD)
 
 PRODUCT_PROPERTY_OVERRIDES += \
